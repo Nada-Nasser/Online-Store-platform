9f0b0005bf25c288716321342c9326700c4cace3
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2020-04-01 20:19:58 +0200

    update usermanager

--- onlineStorePlatform/src/main/java/jar/managers/UserManager.java
+++ onlineStorePlatform/src/main/java/jar/managers/UserManager.java
@@ -16,34 +16,6 @@ import jar.user.User;
 @Service
 public abstract class UserManager {
 	public abstract List<User> getUsers();
-	public abstract boolean isRegisterd(User user);
+	public abstract boolean isRegisterd(User user) throws SQLException;
 	public abstract boolean addUser(User user) throws SQLException;
-
-	/*public boolean isRegisterd(User user) throws SQLException
-	{		
-		Connection connection = DBConnection.getConnectoin();
-		String email = user.getEmail();
-		String Password = user.getPassword();
-		String Name = user.getName();
-		
-		String showSql = "SELECT Name, password, email \r\n" + 
-				"FROM Buyers WHERE email = '" + email + "'  AND\r\n" + 
-				"password = '" + Password + "' AND Name = '" + Name + "';";
-		
-		ResultSet resultSet = null;
-
-		try (
-			PreparedStatement prepsInsertProduct = 
-			connection.prepareStatement(showSql, Statement.RETURN_GENERATED_KEYS);) 
-		{
-			prepsInsertProduct.execute();
-			resultSet = prepsInsertProduct.getResultSet();
-			 
-			if(resultSet.next()){
-		         return true;        
-		     }
-			else
-				return false;
-		}
-	}*/
 }
